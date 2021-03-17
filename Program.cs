863285935054af31293de812f9b71fd0a3b16d1d
bmcgiffin <80338210+bmcgiffin@users.noreply.github.com>
2021-03-17 12:52:13 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -42,15 +42,75 @@ namespace Weather.Forecaster
 
                         JObject joResponse1 = JObject.Parse(messageTask.Result);
 
-                        var joWrite1 = (joResponse1["main"]["temp"]);
-                        var joWrite2 = (joResponse1["wind"]["speed"]);
+                        var joWrite1 = (int)joResponse1["main"]["temp"];
+                        var joWrite2 = joResponse1["wind"]["speed"];
+                        var joWrite3 = (string)joResponse1["weather"][0]["main"];
 
                         Console.WriteLine("The current air temperature in " + userCity + " is: " + joWrite1 + "F" + " and the wind speed is " + joWrite2 + " mph.");
+
+                        string airConditions = joWrite3;
+
+                        switch (airConditions)
+                        {
+
+                            case "Clear":
+                                Console.WriteLine("Currently, in " + userCity + ", it is clear.");
+                                break;
+
+                            case "Haze":
+                                Console.WriteLine("Currently, in " + userCity + ", it is hazey.");
+                                break;
+
+                            case "Clouds":
+                                Console.WriteLine("Currently, in " + userCity + ", it is cloudy.");
+                                break;
+
+                            case "Rain":
+                                Console.WriteLine("Currently, in " + userCity + ", it is rainy.");
+                                break;
+
+                            case "Mist":
+                                Console.WriteLine("Currently, in " + userCity + ", it is misty.");
+                                break;
+
+                            default:
+                                Console.WriteLine("Currently, the air conditions in " + userCity + " is: ");
+                                break;
+                                
+                        }
+
+                        if (joWrite1 < 40)
+                        {
+                            Console.WriteLine("It's pretty chilly today! Better pack a jacket!");
+                                
+                        }
+
+                        else if (joWrite1 > 90)
+                        {
+
+                            Console.WriteLine("It's quite the scorcher today in " + userCity + "! Don't forget your sunscreen!");
+
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Seems like a pretty nice day today. Not too hot, not too cold!");
+                        }
+
+                            
+
                         
+                        
+
+                        
+
+
                     }
+
                     else
+
                     {
-                        Console.WriteLine("Oops! You've entered an invalid city. Please enter a valid city name.");                        
+                        Console.WriteLine("Oops! You've entered an invalid city. Please enter a valid city name.");         
                     }
                 }
             }
